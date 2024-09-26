8286fbc3b0eee67dfb40d3ca9cba6e8722317416
huangfanyi <1345075672@qq.com>
2024-09-26 15:23:26 +0800

    unqiue algorithm

--- unique.hpp
+++ unique.hpp
@@ -7,14 +7,26 @@ namespace meta_base
 {
     template<bool Is_unique,class Template,class Out>
     struct Unique_base
-            :public pair<Is_unique,Out>
-    {};
+    {
+        using type=Out;
+        static constexpr bool value=Is_unique;
+    };
 
     template<bool Is_unique,template<class ...>class Template,class Head,class Empty,class...Rest>
     struct Unique_base<Is_unique,Template<Head,Rest...>,Empty>
             :binary_t<is_contain_v<Empty,Head>(),
             Unique_base<false,Template<Rest...>,Empty>,
             Unique_base<true,Template<Rest...>,merge_t<Empty,Template<Head>>>
+            >
+    {};
+
+    template<bool Is_unique,template<class Value,Value...>class Template,
+            class Empty,
+            class Value_type,Value_type Head,Value_type...Rest>
+    struct Unique_base<Is_unique,Template<Value_type,Head,Rest...>,Empty>
+            :binary_t<is_contain_v<Empty,Head>(),
+                    Unique_base<false,Template<Value_type,Rest...>,Empty>,
+                    Unique_base<true,Template<Value_type,Rest...>,merge_t<Empty,Template<Value_type,Head>>>
             >
     {};
 
@@ -27,22 +39,8 @@ namespace meta_base
     using unique_t=typename unique<Template>::type;
 
     template<class Template>
-    inline constexpr bool is_unique_v=bool(unique<Template>::S_index);
+    inline constexpr bool is_unique_v=bool(unique<Template>::value);
 }
 
-//unique
-namespace meta_base::test
-{
 
-    template<class...>class TypeList{};
-    static_assert(is_same_v<unique_t<TypeList<>>,TypeList<>>);
-    static_assert(is_same_v<unique_t<TypeList<int>>,TypeList<int>>);
-    static_assert(is_same_v<unique_t<TypeList<int>>,TypeList<int>>);
-    static_assert(is_same_v<unique_t<TypeList<int,void,void,int,char,float,float,char>>,TypeList<int,void,char,float>>);
-
-    static_assert(is_unique_v<TypeList<>>);
-    static_assert(is_unique_v<TypeList<int>>);
-    static_assert(is_unique_v<TypeList<int,void,int>> == false);
-    static_assert(is_unique_v<TypeList<int,void,char>> );
-}
 #endif //META_UNIQUE_HPP
