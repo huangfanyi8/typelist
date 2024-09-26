c36f68a7f18ab2b1a791ae59ecd52cdeb530dbd6
huangfanyi <1345075672@qq.com>
2024-09-26 15:58:29 +0800

    test get_n algorithm

--- test/test_get_n.cpp
+++ test/test_get_n.cpp
@@ -1,4 +1,18 @@
+/*
+ * 获取普通模板或变量模板的第n个参数（下标从0开始）
+ * 支持负数下标
+ * 对于普通模板，如果范围超标，则返回undefined
+ * 对于变量模板，返回0
+ *
+ * 预计实现c++20 的CPO模式解决下标越界问题
+ * */
+
 #include"../get_n.hpp"
+#include<variant>
+#include<tuple>
+
+using std::variant;
+using std::tuple;
 
 int main()
 {
@@ -15,7 +29,14 @@ int main()
     static_assert(is_same_v<back_t<variant<int,double,char>>,char>);
     static_assert(is_same_v<back_t<variant<char>>,char>);
     static_assert(is_same_v<back_t<variant<>>,undefined>);
-    static_assert(is_same_v<get_n_t<tuple<int,double,int*,int&,char&,int*>,3>,int&>);
+    
+  static_assert(is_same_v<get_n_t<tuple<int,double,int*,int&,char&,int*>,3>,int&>);
+  static_assert(is_same_v<get_n_t<tuple<int,double,int*,int&,char&,int*>,-3>,int&>);
+  static_assert(is_same_v<get_n_t<tuple<int,double,int*,int&,char&,int*>,-2>,char&>);
+  static_assert(is_same_v<get_n_t<tuple<int,double,int*,int&,char&,int*>,-1>,std::add_pointer_t<int>>);
+  static_assert(is_same_v<get_n_t<tuple<int,double,int*,int&,char&,int*>,-4>,int*>);
+  static_assert(is_same_v<get_n_t<tuple<int,double,int*,int&,char&,int*>,-100>,undefined>);
+  
     static_assert(get_n_v<char_sequence<'1','2','3'>,0> =='1');
     static_assert(get_n_v<char_sequence<'1','2','3'>,1> =='2');
     static_assert(get_n_v<char_sequence<'1','2','3'>,4> ==0);
