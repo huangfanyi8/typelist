63aa0daa69d77a11b14c54be037c147f183b6d86
huangfanyi <1345075672@qq.com>
2024-10-01 16:34:47 +0800

    test sort algorithm

--- test/test_sort.cpp
+++ test/test_sort.cpp
@@ -2,8 +2,11 @@
 #include<tuple>
 #include<iostream>
 
-template<int a,int b>
+template<auto a,auto b>
 using greater=std::bool_constant<(a>b)>;
+
+template<double...v>
+using double_sequence=meta_base::constant_sequence<double,v...>;
 
 int main()
 {
@@ -13,7 +16,7 @@ int main()
   std::cout<<meta_base::is_same_v<meta_base::insertion_sort_t<std::tuple<char,int,double>,
     meta_base::greater>,std::tuple<double,int,char>>;
   
-  static_assert(meta_base::is_same_v<meta_base::insertion_sort_t<std::tuple<char,int,float>,
+  static_assert(meta_base::is_same_v<meta_base::insertion_sort_t<std::tuple<int,float,char>,
     meta_base::greater>,std::tuple<int,float,char>>);
   
   std::cout<<meta_base::is_same_v<meta_base::insertion_sort_t<std::tuple<>,
@@ -22,12 +25,18 @@ int main()
   std::cout<<meta_base::is_same_v<meta_base::insertion_sort_t<std::tuple<char,int,double>,
     meta_base::less>,std::tuple<double,int,char>>;
   
-  static_assert(meta_base::is_same_v<meta_base::insertion_sort_t<std::tuple<char,int,float>,
+  static_assert(meta_base::is_same_v<meta_base::insertion_sort_t<std::tuple<char,float,int>,
     meta_base::less>,std::tuple<char,float,int>>);
   
-  static_assert(meta_base::is_same_v<meta_base::int_sequence<>,
-    meta_base::insertion_sort_c_t<meta_base::int_sequence<>,greater>>);
+  static_assert(meta_base::is_same_v<meta_base::insertion_sort_t<std::tuple<char>,
+    meta_base::less>,std::tuple<char>>);
   
-  static_assert(meta_base::is_same_v<meta_base::int_sequence<7,6,5,4,3,2,1,0>,
-    meta_base::insertion_sort_c_t<meta_base::int_sequence<1,2,3,4,5,7,6,0>,greater>>);
+  static_assert(meta_base::is_same_v<double_sequence<>,
+    meta_base::experimental::insertion_sort_t<double_sequence<>,greater>
+    >);
+  
+  static_assert(meta_base::is_same_v<
+    double_sequence<5.00,5.0,2.0,1.0>,
+    meta_base::experimental::insertion_sort_t<double_sequence<1.0,2.00,5.00,5.0>,greater>
+  >);
 }
